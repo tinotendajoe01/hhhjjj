95f8973ab974f0779abdd4ccf32b7af145bb168a
tinotendajoe01 <tinotendajoe01@gmail.com>
2022-01-15 13:10:49 +0200

    google api

--- src/pages/api/keys/google.js
+++ src/pages/api/keys/google.js
@@ -1,10 +1,10 @@
-import nc from 'next-connect';
-import { isAuth } from '../../../utils/auth';
+import nc from "next-connect";
+import { isAuth } from "../../../../utils/auth";
 
 const handler = nc();
 handler.use(isAuth);
 handler.get(async (req, res) => {
-  res.send(process.env.GOOGLE_API_KEY || 'nokey');
+  res.send(process.env.GOOGLE_API_KEY || "nokey");
 });
 
 export default handler;
