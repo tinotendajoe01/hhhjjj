1628d1d687c63ec935280b320c8c2f43eac6cf72
tinotendajoe01 <tinotendajoe01@gmail.com>
2022-01-27 14:44:20 +0200

    hh

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
-
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes npm packages that depend on `fs` module
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: "empty",
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 
   images: {
     domains: [
