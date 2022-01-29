0499f1b391c2fa7602a6bd18c1292f030e613c72
tinotendajoe01 <tinotendajoe01@gmail.com>
2022-01-29 11:17:33 +0200

    mongoose

--- utils/db.js
+++ utils/db.js
@@ -1,40 +1,78 @@
+// import mongoose from "mongoose";
+// require("dotenv").config();
+// // require("dotenv").config({ path: "ENV_FILENAME" });
+// // import 'dotenv/config'
+// const connection = {};
+
+// async function connect() {
+//   if (connection.isConnected) {
+//     console.log("already connected");
+//     return;
+//   }
+//   if (mongoose.connections.length > 0) {
+//     connection.isConnected = mongoose.connections[0].readyState;
+//     if (connection.isConnected === 1) {
+//       console.log("use previous connection");
+//       return;
+//     }
+//     await mongoose.disconnect();
+//   }
+//   const db = await mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+//   console.log("new connection");
+//   connection.isConnected = db.connections[0].readyState;
+// }
+
+// async function disconnect() {
+//   if (connection.isConnected) {
+//     if (process.env.NODE_ENV === "production") {
+//       await mongoose.disconnect();
+//       connection.isConnected = false;
+//     } else {
+//       console.log("not disconnected");
+//     }
+//     // d4v3@!07
+//   }
+// }
 import mongoose from "mongoose";
 require("dotenv").config();
-// require("dotenv").config({ path: "ENV_FILENAME" });
-// import 'dotenv/config'
-const connection = {};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-async function connect() {
-  if (connection.isConnected) {
-    console.log("already connected");
-    return;
-  }
-  if (mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState;
-    if (connection.isConnected === 1) {
-      console.log("use previous connection");
-      return;
-    }
-    await mongoose.disconnect();
-  }
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log("new connection");
-  connection.isConnected = db.connections[0].readyState;
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
-async function disconnect() {
-  if (connection.isConnected) {
-    if (process.env.NODE_ENV === "production") {
-      await mongoose.disconnect();
-      connection.isConnected = false;
-    } else {
-      console.log("not disconnected");
-    }
-    // d4v3@!07
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function db() {
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 function convertDocToObj(doc) {
